1b253f488664074681f449f542a01a34bbadf6f9
Миронов Владислав <vlad.banana.syca@yandex.ru>
2023-08-27 15:04:38 +0300

    фыв

--- ShiftCalculator/PerformanceFolder/PageFolder/ProductPage.xaml.cs
+++ ShiftCalculator/PerformanceFolder/PageFolder/ProductPage.xaml.cs
@@ -2,8 +2,6 @@
 /// Страница, для подсчёта накенки товара
 ///-->
 
-using ShiftCalculator.AppDataFolder.ClassFolder;
-using System;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -11,127 +9,55 @@ namespace ShiftCalculator.PerformanceFolder.PageFolder
 {
     public partial class ProductPage : Page
     {
-        string mainNamePage = "ProductPage:\n\n";
-
         public ProductPage()
         {
-            try
-            {
-                InitializeComponent();
-            }
-            catch (Exception ex)
-            {
-                MessageBoxClass.ExceptionMessageBox_MBC(textMessage: $"Событие ProductPage в {mainNamePage}" + $"{ex.Message}");
-            }
+            InitializeComponent();
         }
         #region Текстовые подсказки
         private void ExtraChargeTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            try
-            {
-                
-            }
-            catch (Exception ex)
-            {
-                MessageBoxClass.ExceptionMessageBox_MBC(textMessage: $"Событие ExtraChargeTextBox_TextChanged в {mainNamePage}" + $"{ex.Message}");
-            }
+
         }
 
         private void QuantityTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            try
-            {
-                
-            }
-            catch (Exception ex)
-            {
-                MessageBoxClass.ExceptionMessageBox_MBC(textMessage: $"Событие QuantityTextBox_TextChanged в {mainNamePage}" + $"{ex.Message}");
-            }
+
         }
 
         private void CostTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            try
-            {
-               
-            }
-            catch (Exception ex)
-            {
-                MessageBoxClass.ExceptionMessageBox_MBC(textMessage: $"Событие CostTextBox_TextChanged в {mainNamePage}" + $"{ex.Message}");
-            }
+
         }
 
         private void ResultTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            try
-            {
-                
-            }
-            catch (Exception ex)
-            {
-                MessageBoxClass.ExceptionMessageBox_MBC(textMessage: $"Событие ResultTextBox_TextChanged в {mainNamePage}" + $"{ex.Message}");
-            }
+
         }
         #endregion
         #region _Click
         private void HintExtraChargeButton_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                
-            }
-            catch (Exception ex)
-            {
-                MessageBoxClass.ExceptionMessageBox_MBC(textMessage: $"Событие HintExtraChargeButton_Click в {mainNamePage}" + $"{ex.Message}");
-            }
+
         }
 
         private void HintQuantityButton_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                
-            }
-            catch (Exception ex)
-            {
-                MessageBoxClass.ExceptionMessageBox_MBC(textMessage: $"Событие HintQuantityButton_Click в {mainNamePage}" + $"{ex.Message}");
-            }
+
         }
 
         private void HintCostButton_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                
-            }
-            catch (Exception ex)
-            {
-                MessageBoxClass.ExceptionMessageBox_MBC(textMessage: $"Событие HintCostButton_Click в {mainNamePage}" + $"{ex.Message}");
-            }
+
         }
 
         private void HintResultButton_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                
-            }
-            catch (Exception ex)
-            {
-                MessageBoxClass.ExceptionMessageBox_MBC(textMessage: $"Событие HintResultButton_Click в {mainNamePage}" + $"{ex.Message}");
-            }
+
         }
 
         private void CalculateButton_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                
-            }
-            catch (Exception ex)
-            {
-                MessageBoxClass.ExceptionMessageBox_MBC(textMessage: $"Событие CalculateButton_Click в {mainNamePage}" + $"{ex.Message}");
-            }
+
         }
         #endregion
         #region Event_
