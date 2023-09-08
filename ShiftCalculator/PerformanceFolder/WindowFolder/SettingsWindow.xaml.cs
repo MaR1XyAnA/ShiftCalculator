957f9e06342b77f3fc1fdfaaf7ad442fc2843612
Миронов Владислав <vlad.banana.syca@yandex.ru>
2023-09-08 22:59:24 +0300

    sdf

--- ShiftCalculator/PerformanceFolder/WindowFolder/SettingsWindow.xaml.cs
+++ ShiftCalculator/PerformanceFolder/WindowFolder/SettingsWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using ShiftCalculator.Properties;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace ShiftCalculator.PerformanceFolder.WindowFolder
@@ -8,6 +12,7 @@ namespace ShiftCalculator.PerformanceFolder.WindowFolder
         public SettingsWindow()
         {
             InitializeComponent();
+            Event_AssignTheCurrentPathTextBox();
         }
         #region Управление окном
         private void TopShelGrid_MouseDown(object sender, MouseButtonEventArgs e) /// Перемищать окно
@@ -23,6 +28,73 @@ namespace ShiftCalculator.PerformanceFolder.WindowFolder
         private void CloseWindowButton_Click(object sender, RoutedEventArgs e) /// Закрыть окно
         {
             this.Close();
+        }
+        #endregion
+        #region Event_
+        private void Event_AssignTheCurrentPathTextBox()
+        {
+            ThePathToTheFileForSavingTheHistoryOfTheMarkupTextBox.Text = Settings.Default.ThePathToTheFileForSavingTheHistoryOfTheMarkup;
+            ThePathToTheFileToSaveTheShiftCountingHistoryTextBox.Text = Settings.Default.ThePathToTheFileToSaveTheShiftCountingHistory;
+        }
+        #endregion
+        #region _TextChanged
+        private void HintText_TextChanged(object sender, TextChangedEventArgs e) ///Текстовые подсказки
+        {
+            TextBox textBox = (TextBox)sender;
+            TextBlock hintTextBlock = null;
+
+            switch (textBox.Name)
+            {
+                case "ThePathToTheFileForSavingTheHistoryOfTheMarkupTextBox":
+                    hintTextBlock = ThePathToTheFileForSavingTheHistoryOfTheMarkupTextBlock;
+                    break;
+                case "ThePathToTheFileToSaveTheShiftCountingHistoryTextBox":
+                    hintTextBlock = ThePathToTheFileToSaveTheShiftCountingHistoryTextBlock;
+                    break;
+            }
+
+            if (textBox.Text.Length == 0)
+            {
+                hintTextBlock.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                hintTextBlock.Visibility = Visibility.Collapsed;
+            }
+        }
+        #endregion
+        #region _Click
+        private void AssignTheCurrentPath_Click(object sender, RoutedEventArgs e)
+        {
+            // Создаем диалоговое окно для выбора файла
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = false;
+            openFileDialog.Filter = "JSON Files (*.json)|*.json|All Files (*.*)|*.*";
+
+            Button button = (Button)sender;
+
+            // Словарь, сопоставляющий кнопки и настройки
+            Dictionary<Button, string> buttonSettingsMap = new Dictionary<Button, string>
+            {
+                { ThePathToTheFileForSavingTheHistoryOfTheMarkupButton, "ThePathToTheFileForSavingTheHistoryOfTheMarkup" },
+                { ThePathToTheFileToSaveTheShiftCountingHistoryButton, "ThePathToTheFileToSaveTheShiftCountingHistory" }
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // Получаем выбранный путь и сохраняем его в настройках
+                string selectedFilePath = openFileDialog.FileName;
+
+                if (buttonSettingsMap.ContainsKey(button))
+                {
+                    string settingKey = buttonSettingsMap[button];
+
+                    Settings.Default[settingKey] = selectedFilePath;
+                    Settings.Default.Save();
+
+                    Event_AssignTheCurrentPathTextBox(); 
+                }
+            }
         }
         #endregion
     }
