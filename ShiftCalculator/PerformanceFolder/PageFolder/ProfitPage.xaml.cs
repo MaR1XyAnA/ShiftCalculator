c22b9a480e5ad118677b2f0111e2fab4899d87d0
Миронов Владислав <vlad.banana.syca@yandex.ru>
2023-10-22 18:28:07 +0300

    123

--- ShiftCalculator/PerformanceFolder/PageFolder/ProfitPage.xaml.cs
+++ ShiftCalculator/PerformanceFolder/PageFolder/ProfitPage.xaml.cs
@@ -1,9 +1,9 @@
 ﻿///<!--
-/// Страница предстовляет из себя специальный калькулятор с журналом.
-/// Пользователю необходимо ввести определённые данные, после чего они попадут в специальный класс, где произойдёт счёт
-///     и класс выдаст значения, которые получит пользователь. 
+/// Страница представляет из себя специальный калькулятор с журналом.
+/// Пользователю необходимо ввести определённые данные, после чего они попадут в специальный класс, ...  
+///  ... где произойдёт счёт и класс выдаст значения, которые получит пользователь. 
 /// После значения записываются в журнал.
-/// Журнал - это обычный txt файл, куда определённым образом просто записываются данные и всё.
+/// Журнал - это обычный .json файл, куда определённым образом просто записываются данные и всё.
 ///-->
 
 using Newtonsoft.Json;
@@ -25,25 +25,34 @@ namespace ShiftCalculator.PerformanceFolder.PageFolder
 {
     public partial class ProfitPage : Page
     {
+        // Выбранные элементы
         List<HistoryClass> selectedItemList = new List<HistoryClass>();
-        List<HistoryClass> saveNewList = new List<HistoryClass>();
 
-        string isNullOrWhiteSpaceTextBox;
+        // Проверка полей на пустоту "нужно для метода"
+        string isNullOrWhiteSpaceTextBox; 
 
         DispatcherTimer dispatcherTimer;
 
         public ProfitPage()
         {
+            /// Пометка для тех, кто будет читать мой код и не поймёт прикола, ...
+            /// ... а так же для новичков и для меня, если я там например уйду в армию
+            
+            // Так как при запуске страницы срабатывает код написанный в данном методе, то я разделил код на ...
+            // ... события и запускаю события, которые запускают код (иначе код просто будет длинным)
+            // InitializeComponent() - стоковое событие, я его не прописывал.
+
             InitializeComponent();
             Event_OutputData();
             Event_SettingsDispatcherTimer();
             Event_ToggleDateTimeControls(true);
 
-            HistoryDataGrid.Items.SortDescriptions.Add(new SortDescription("LineNumber_HC", ListSortDirection.Descending));
+            // А это просто сортировка по дате и времени (От самого нового к старому)
+            HistoryDataGrid.Items.SortDescriptions.Add(new SortDescription("DateTime_HC", ListSortDirection.Descending));
         }
 
         #region _TextChanged
-        private void TextHint_TextChanged(object sender, TextChangedEventArgs e) ///Показать\Спрятать текстовую подсказку
+        private void TextHint_TextChanged(object sender, TextChangedEventArgs e) /// Показать\Спрятать текстовую подсказку
         {
             TextBox textBox = (TextBox)sender;
             TextBlock hintTextBlock = null;
@@ -81,7 +90,7 @@ namespace ShiftCalculator.PerformanceFolder.PageFolder
         }
         #endregion
         #region _Click
-        private void SetToDayDateTime_Click(object sender, RoutedEventArgs e)
+        private void SetToDayDateTime_Click(object sender, RoutedEventArgs e) /// В реальном времени
         {
             if (SetToDayDateTime.IsChecked == true)
             {
@@ -93,8 +102,10 @@ namespace ShiftCalculator.PerformanceFolder.PageFolder
             }
         }
 
-        private void CalculateButton_Click(object sender, RoutedEventArgs e) /// Работа кнопки
+        private void CalculateButton_Click(object sender, RoutedEventArgs e) /// Работа основной кнопки
         {
+            // 1. Проверяем поля на пустату
+            // 2. В зависемости от результата, выполняем действие
             isNullOrWhiteSpaceTextBox = "";
 
             Event_IsNullOrWhiteSpaceTextBox();
@@ -109,29 +120,30 @@ namespace ShiftCalculator.PerformanceFolder.PageFolder
             }
         }
 
-        private void DeleteAnEntryButton_Click(object sender, RoutedEventArgs e)
+        private void DeleteAnEntryButton_Click(object sender, RoutedEventArgs e) /// Удаление
         {
 
         }
 
-        private void ResetSelectionButton_Click(object sender, RoutedEventArgs e)
+        private void ResetSelectionButton_Click(object sender, RoutedEventArgs e) /// Сбросить выделение
         {
             HistoryDataGrid.SelectedItem = null;
         }
 
-        private void CopyButton_Click(object sender, RoutedEventArgs e)
+        private void CopyButton_Click(object sender, RoutedEventArgs e) /// Скопировать
         {
 
         }
 
-        private void DownloadButton_Click(object sender, RoutedEventArgs e)
+        private void DownloadButton_Click(object sender, RoutedEventArgs e) /// Скачать
         {
 
         }
         #endregion
         #region Event_
-        private void Event_ToggleDateTimeControls(bool enable)
+        private void Event_ToggleDateTimeControls(bool enable) /// Дата и время
         {
+            //В зависимости от положения кнопки, таймер вкл\выкл + спец поля вкл\выкл
             DateTextBox.IsEnabled = !enable;
             TimeTextBox.IsEnabled = !enable;
 
@@ -151,7 +163,7 @@ namespace ShiftCalculator.PerformanceFolder.PageFolder
             dispatcherTimer.Interval = TimeSpan.FromMilliseconds(1);
         }
 
-        private void Event_DispatcherTimer(object sender, EventArgs e) /// Событие для таймера
+        private void Event_DispatcherTimer(object sender, EventArgs e) /// Событие таймера
         {
             DateTime toDaytime = DateTime.Now;
 
@@ -173,7 +185,7 @@ namespace ShiftCalculator.PerformanceFolder.PageFolder
 
             if (string.IsNullOrWhiteSpace(PreviousCashBalanceTextBox.Text))
             {
-                isNullOrWhiteSpaceTextBox += "● Текстовое поле \"Предыдуший кассовый остаток\" не должно быть пустым.\n\n";
+                isNullOrWhiteSpaceTextBox += "● Текстовое поле \"Предыдущий кассовый остаток\" не должно быть пустым.\n\n";
             }
 
             if (string.IsNullOrWhiteSpace(BanckTextBox.Text))
@@ -182,17 +194,18 @@ namespace ShiftCalculator.PerformanceFolder.PageFolder
             }
         }
 
-        private void Event_CheckingBeforeRecording()
+        private void Event_CheckingBeforeRecording() /// Сохранение результатов
         {
+            // В зависимости от состояния кнопки (вкл\выкл) и событий (дата и время) ...
+            // ... выполняем \ не выполняем сохранение
             if (SaveToggleButton.IsChecked == true)
             {
-                string dateFormat = "dd.MM.yyyy";
                 DateTime dateResult;
                 TimeSpan timeResult;
 
                 if (SetToDayDateTime.IsChecked == false)
                 {
-                    if (DateTime.TryParseExact(DateTextBox.Text, dateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out dateResult))
+                    if (DateTime.TryParseExact(DateTextBox.Text, "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dateResult))
                     {
                         if (TimeSpan.TryParse(TimeTextBox.Text, out timeResult))
                         {
@@ -253,10 +266,22 @@ namespace ShiftCalculator.PerformanceFolder.PageFolder
                 JsonConvert.SerializeObject(historyClass, Formatting.Indented));
         }
 
-        private void Event_OutputData()
+        private void Event_OutputData() // Вывод истории подсчётов
         {
-            string json = File.ReadAllText(Settings.Default.ThePathToTheFileForSavingTheHistoryOfTheMarkup);
-            HistoryDataGrid.ItemsSource = JsonConvert.DeserializeObject<List<HistoryClass>>(json);
+            // Проверяем существование файла
+            if (File.Exists(Settings.Default.ThePathToTheFileForSavingTheHistoryOfTheMarkup))
+            {
+                string json = File.ReadAllText(Settings.Default.ThePathToTheFileForSavingTheHistoryOfTheMarkup);
+                HistoryDataGrid.ItemsSource = JsonConvert.DeserializeObject<List<HistoryClass>>(json);
+            }
+            else
+            {
+                List<HistoryClass> emptyHistory = new List<HistoryClass>();
+                string emptyJson = JsonConvert.SerializeObject(emptyHistory);
+
+                File.WriteAllText(Settings.Default.ThePathToTheFileForSavingTheHistoryOfTheMarkup, emptyJson);
+                HistoryDataGrid.ItemsSource = emptyHistory;
+            }
         }
 
         private void Event_PerformCountingOperation()
@@ -290,8 +315,8 @@ namespace ShiftCalculator.PerformanceFolder.PageFolder
             e.Handled = DivisionCodeRegex.IsMatch(e.Text);
         }
         #endregion
-
-        private void HistoryDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        #region _SelectionChanged
+        private void HistoryDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e) /// Позволяет элемент или список элементов при выделении запомнить и работать
         {
             foreach (var selectedItem in HistoryDataGrid.SelectedItems)
             {
@@ -316,5 +341,6 @@ namespace ShiftCalculator.PerformanceFolder.PageFolder
                 DownloadButton.IsEnabled = false;
             }
         }
+        #endregion
     }
 }
